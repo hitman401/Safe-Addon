77ca5aedc4a2e5f0335a043ef58745479e82836d
ustulation <ustulation@gmail.com>
2015-08-12 12:03:43 +0530

    FireFox js-ctypes integrated with all tests passing

--- index.js
+++ index.js
@@ -5,11 +5,66 @@ Cu.import("resource://gre/modules/Services.jsm");
 var sdkSelf = require('sdk/self');
 // This can be expanded to show some settings page to register & unregister safe: protocol handling, probably much more
 var handleIconClick = function(state) {
-  //require("sdk/tabs").open("http://www.maidsafe.net/");
-  //var nativeUrl = Services.io.newURI(sdkSelf.data.url("libc_wrapper.dll"),null,null).QueryInterface(Ci.nsIFileURL).file.path;
-  var lib = ctypes.open("k:\\\\Maidsafe\\\\frontend\\\\git\\\\Safe-Addon\\\libc_wrapper.dll");
-  //var getSize = lib.declare
-  console.log("read");
+  var lib = ctypes.open("./libc_wrapper.so");
+
+  var c_create_sub_directory = lib.declare('c_create_sub_directory',
+                                           ctypes.default_abi,
+                                           ctypes.int32_t,
+                                           ctypes.char.ptr,
+                                           ctypes.bool);
+
+  var c_create_file = lib.declare('c_create_file',
+                                  ctypes.default_abi,
+                                  ctypes.int32_t,
+                                  ctypes.char.ptr,
+                                  ctypes.char.ptr);
+
+  var c_get_file_size = lib.declare('c_get_file_size',
+                                    ctypes.default_abi,
+                                    ctypes.int32_t,
+                                    ctypes.char.ptr,
+                                    ctypes.int32_t.ptr);
+
+  var c_get_file_content = lib.declare('c_get_file_content',
+                                       ctypes.default_abi,
+                                       ctypes.int32_t,
+                                       ctypes.char.ptr,
+                                       ctypes.char.ptr);
+
+  console.log("=========== Test Start =============");
+  console.log('Creating sub-directory "/zero" ...');
+  var error_code = c_create_sub_directory("/zero", false);
+  if (error_code == 0) console.log("Successful !");
+  else console.log("Error-code:", error_code);
+
+  console.log('Creating sub-directory "/zero/one" ...');
+  error_code = c_create_sub_directory("/zero/one", false);
+  if (error_code == 0) console.log("Successful !");
+  else console.log("Error-code:", error_code);
+
+  console.log('Creating file "/zero/one/INDEX.html" with content "This is index.html" ...');
+  error_code = c_create_file("/zero/one/INDEX.html", "This is index.html");
+  if (error_code == 0) console.log("Successful !");
+  else console.log("Error-code:", error_code);
+
+  console.log('Getting size for file "/zero/one/INDEX.html" ...');
+  let file_size = ctypes.int32_t(-1);
+  error_code = c_get_file_size("/zero/one/INDEX.html", file_size.address());
+  if (error_code == 0) {
+    console.log("File size in bytes:", file_size.value);
+    console.log("Successful !");
+  } else console.log("Error-code:", error_code);
+
+  console.log('Getting contents of file "/zero/one/INDEX.html" ...');
+  let CharArray_t = ctypes.ArrayType(ctypes.char, file_size.value + 1);
+  let file_content = CharArray_t();
+  error_code = c_get_file_content("/zero/one/INDEX.html", file_content.addressOfElement(0));
+  if (error_code == 0) {
+    console.log("File content:", file_content.readString());
+    console.log("Successful !");
+  } else console.log("Error-code:", error_code);
+  console.log("=========== Test End =============")
+
   lib.close();
 };
 
